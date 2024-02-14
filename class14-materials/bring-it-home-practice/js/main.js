273c791d86051c087497d47c6eda5c23b134fe2d
violet8969 <155793460+violet8969@users.noreply.github.com>
2024-02-14 18:13:51 -0500

    class 14

--- class14-materials/bring-it-home-practice/js/main.js
+++ class14-materials/bring-it-home-practice/js/main.js
@@ -1,14 +1,32 @@
 // *Variables*
 // Create a variable and console log the value
-
+let soupTime = "Miso"
+console.log(soupTime)
 // Create a variable, add 10 to it, and alert the value
-
+let proteinAmount = 5
+proteinAmount = proteinAmount+ 10
+alert(proteinAmount)
 // *Functions*
 // Create a function that subtracts 4 numbers and alerts the difference
-
+function substactFourNumbers(num1, num2, num3, num4){
+    let subtractResult = num1 - num2 - num3 - num4
+    alert(subtractResult)
+}
+substactFourNumbers(50, 10, 5, 1)
+    
 // Create a function that divides one number by another and returns the remainder
+function divideAndReturn(n1,n2){
+    return n1/n2
 
+}
+alert(divideAndReturn(50, 2))
 // *Conditionals*
 // Create a function that adds two numbers and if the sum is greater than 50 alert Jumanji
+function jumanjiAdd(v1,v2){
+    const addTwo = v1 + v2
 
+    if (addTwo > 50)
+    alert("Jumanji") 
+}
+jumanjiAdd(22,30)
 // Create a function that multiplys three numbers and if the product is divisible by 3 alert ZEBRA
--- class14-materials/simple-functions/js/main.js
+++ class14-materials/simple-functions/js/main.js
@@ -1,5 +1,22 @@
 //create a function that adds two numbers and alerts the sum
+function addTwoNumbers(num1, num2){
+    alert(num1+num2)
+}
 
+addTwoNumbers(2,3)
 //create a function that multiplys three numbers and console logs the product
+ 
+function multiplyThree(zeb1, zeb2, zeb3){
+    const product = zeb1 * zeb2 * zeb3
+    console.log(product)
+}
+
+multiplyThree(2, 3, 4)
 
 //create a function that divides two numbers and returns the ???
+
+function dividesTwoNumbers (n1, n2){
+    return n1/n2
+}
+
+console.log(dividesTwoNumbers(12, 6))
